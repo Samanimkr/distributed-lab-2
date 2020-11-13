1ae4bc7f16c15b54506073306a8f9e1ee84d0e6c
Samanimkr <Samanimkr@Outlook.com>
2020-11-13 17:35:01 +0000

    Setup an RPC

--- secretstrings/client/client.go
+++ secretstrings/client/client.go
@@ -1,16 +1,40 @@
 package main
+
 import (
-//	"net/rpc"
+	"bufio"
 	"flag"
-//	"bufio"
-//	"os"
-//	"secretstrings/stubs"
 	"fmt"
+	"net/rpc"
+	"os"
+	"secretstrings/stubs"
 )
 
-func main(){
-	server := flag.String("server","127.0.0.1:8030","IP:port string to connect to as server")
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
 	flag.Parse()
 	fmt.Println("Server: ", *server)
-	//TODO: connect to the RPC server and send the request(s)
+
+	client, _ := rpc.Dial("tcp", *server)
+	defer client.Close()
+
+	// Read the words from wordlist
+	f, err := os.Open("../wordlist")
+	check(err)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		msg := scanner.Text()
+		request := stubs.Request{Message: msg}
+		response := new(stubs.Response)
+
+		client.Call(stubs.PremiumReverseHandler, request, response)
+		fmt.Println("Request: " + request.Message)
+		fmt.Println("Responded: " + response.Message)
+	}
 }
